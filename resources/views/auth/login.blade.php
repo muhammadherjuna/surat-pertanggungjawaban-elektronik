ae415dcf29fae7af682b4c48251848bf0d22d6cb
Muhammad Herjuna <trahjuna1416@gmail.com>
2026-06-12 05:46:05 +0700

    Customize login view into Indonesian and set AdminLTE global app name to SPJ Elektronik

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,1 +1,105 @@
-@extends('adminlte::auth.login')
+@extends('adminlte::auth.auth-page', ['authType' => 'login'])
+
+@section('adminlte_css_pre')
+    <link rel="stylesheet" href="{{ asset('vendor/icheck-bootstrap/icheck-bootstrap.min.css') }}">
+@stop
+
+@php
+    $loginUrl = View::getSection('login_url') ?? config('adminlte.login_url', 'login');
+    $registerUrl = View::getSection('register_url') ?? config('adminlte.register_url', 'register');
+    $passResetUrl = View::getSection('password_reset_url') ?? config('adminlte.password_reset_url', 'password/reset');
+
+    if (config('adminlte.use_route_url', false)) {
+        $loginUrl = $loginUrl ? route($loginUrl) : '';
+        $registerUrl = $registerUrl ? route($registerUrl) : '';
+        $passResetUrl = $passResetUrl ? route($passResetUrl) : '';
+    } else {
+        $loginUrl = $loginUrl ? url($loginUrl) : '';
+        $registerUrl = $registerUrl ? url($registerUrl) : '';
+        $passResetUrl = $passResetUrl ? url($passResetUrl) : '';
+    }
+@endphp
+
+@section('auth_header', 'Silakan masuk untuk memulai sesi Anda')
+
+@section('auth_body')
+    <form action="{{ $loginUrl }}" method="post">
+        @csrf
+
+        {{-- Email field --}}
+        <div class="input-group mb-3">
+            <input type="email" name="email" class="form-control @error('email') is-invalid @enderror"
+                value="{{ old('email') }}" placeholder="Alamat Email" autofocus>
+
+            <div class="input-group-append">
+                <div class="input-group-text">
+                    <span class="fas fa-envelope {{ config('adminlte.classes_auth_icon', '') }}"></span>
+                </div>
+            </div>
+
+            @error('email')
+                <span class="invalid-feedback" role="alert">
+                    <strong>{{ $message }}</strong>
+                </span>
+            @enderror
+        </div>
+
+        {{-- Password field --}}
+        <div class="input-group mb-3">
+            <input type="password" name="password" class="form-control @error('password') is-invalid @enderror"
+                placeholder="Kata Sandi">
+
+            <div class="input-group-append">
+                <div class="input-group-text">
+                    <span class="fas fa-lock {{ config('adminlte.classes_auth_icon', '') }}"></span>
+                </div>
+            </div>
+
+            @error('password')
+                <span class="invalid-feedback" role="alert">
+                    <strong>{{ $message }}</strong>
+                </span>
+            @enderror
+        </div>
+
+        {{-- Login field --}}
+        <div class="row">
+            <div class="col-7">
+                <div class="icheck-primary" title="Ingat Saya">
+                    <input type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
+
+                    <label for="remember">
+                        Ingat Saya
+                    </label>
+                </div>
+            </div>
+
+            <div class="col-5">
+                <button type=submit class="btn btn-block {{ config('adminlte.classes_auth_btn', 'btn-flat btn-primary') }}">
+                    <span class="fas fa-sign-in-alt"></span>
+                    Masuk
+                </button>
+            </div>
+        </div>
+    </form>
+@stop
+
+@section('auth_footer')
+    {{-- Password reset link --}}
+    @if($passResetUrl)
+        <p class="my-0">
+            <a href="{{ $passResetUrl }}">
+                Lupa Kata Sandi?
+            </a>
+        </p>
+    @endif
+
+    {{-- Register link --}}
+    @if($registerUrl)
+        <p class="my-0">
+            <a href="{{ $registerUrl }}">
+                Daftar Akun Baru
+            </a>
+        </p>
+    @endif
+@stop
